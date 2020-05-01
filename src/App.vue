d03b6872c0f15bcdf426832b50881644ce542a88
Joao_MFF <joao_ff@hotmail.com>
2020-05-01 11:41:29 +0200

    CHANGED: Dark Theme toggle and search input to top bar ADDED: Dark Theme variables ADDED: Cards for every added user

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,67 @@
 <template>
   <div id="app">
-    <div class="q-pa-md doc-container">
-      <div class="column items-center">
-        <div class="col-8">
+    <!-- top div  -->
+    <div class="q-pa-md">
+      <div class="row justify-around">
+        <div class="col-4">
           <q-toggle v-model="isDarkTheme" label="Dark Theme"></q-toggle>
+        </div>
+        <div class="col-2">
           <q-input
-            dense square outlined :filled="isDarkTheme"
-            class="search" v-model="searchTxt" label="Search user">
+            dense square outlined class="search" v-model="searchTxt" label="Search user">
             <template v-slot:append>
               <q-icon v-if="searchTxt !== ''" name="close" @click="searchTxt = ''" class="cursor-pointer"></q-icon>
               <q-icon name="search"></q-icon>
             </template>
           </q-input>
         </div>
-        <div class="col">
-          <q-form @submit.prevent="onSubmit" class="q-gutter-md" ref="myForm">
-            <q-input
-              ref="login"
-              square
-              outlined
-              :filled="isDarkTheme"
-              v-model="login"
-              label="Login"
-              lazy-rules
-              :rules="[
-                val => val && val.length || 'Please type something',
+      </div>
+    </div>
+    <!-- End top div  -->
+    <!-- Form   -->
+    <div class="q-pa-md">
+      <div class="row justify-center">
+        <div class="col-12 col-md-auto">
+          <q-card>
+            <q-form @submit.prevent="onSubmit" class="q-gutter-md" ref="myForm">
+              <q-input
+                ref="login" square outlined
+                v-model="login" label="Login" lazy-rules
+                :rules="[
+                val => val && val.length || errRequired,
                 val => val && !users.find(user => user.name === val) || 'Name already exists'
               ]"
-            />
+              />
 
-            <q-input
-              ref="email"
-              square
-              outlined
-              autogrow
-              :filled="isDarkTheme"
-              v-model="emails"
-              label="Emails (comma separated)"
-              lazy-rules
-              :rules="[ val => val && val.length || 'Please type something']"
-            />
-            <div>
-              <q-btn label="Submit" type="submit"></q-btn>
-            </div>
-          </q-form>
+              <q-input
+                ref="email" square outlined autogrow
+                v-model="emails" label="Emails (comma separated)" lazy-rules
+                :rules="[ val => val && val.length || errRequired]"
+              />
+              <div>
+                <q-btn label="Submit" type="submit"></q-btn>
+              </div>
+            </q-form>
+          </q-card>
         </div>
+      </div>
+    </div>
+    <!--  End Form  -->
+    <div v-if="users.length">
+      <div class="q-pa-md row">
+        <h4>Users</h4>
+      </div>
+      <div class="q-pa-md row items-start q-gutter-md ">
+        <q-card v-for="user in users" :key="user.name" class="user-cards">
+          <q-card-section>
+            <div class="text-h6">Name: <span>{{user.name}}</span></div>
+            <div class="text-h6 emails">Emails:
+              <ul>
+                <li v-for="email in user.emails" :key="user.name + '_' + email">{{email}}</li>
+              </ul>
+            </div>
+          </q-card-section>
+        </q-card>
       </div>
     </div>
   </div>
@@ -72,10 +89,10 @@
         users: [],
         isDarkTheme: false,
         countryList: countryListJson,
+        errRequired: 'Field is required'
       }
     },
-    computed: {
-    },
+    computed: {},
     methods: {
       onSubmit() {
         this.$refs.myForm.validate();
--- src/quasar.js
+++ src/quasar.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 import './styles/quasar.scss'
 import '@quasar/extras/material-icons/material-icons.css'
-import {Quasar, QInput, QToggle, QForm} from 'quasar'
+import {Quasar, QInput, QToggle, QForm, QCard, QCardSection} from 'quasar'
 
 Vue.use(Quasar, {
   config: {},
@@ -10,6 +10,8 @@ Vue.use(Quasar, {
     QInput ,
     QToggle,
     QForm,
+    QCard,
+    QCardSection,
   },
   directives: { /* not needed if importStrategy is not 'manual' */ },
   plugins: {
